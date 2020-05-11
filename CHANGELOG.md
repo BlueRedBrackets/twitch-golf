451f98f80622cb036198f18cbea0db20da16dd7d
blueredbrackets <blueredbrackets@gmail.com>
2020-05-11 18:59:10 -0500

    added timer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,11 +4,16 @@ All notable changes to the "twitch-golf" extension will be documented in this fi
 
 ## [Unreleased]
 ### General
-- **twitch chat feed**
-- **send message to chat**
+- twitch chat feed
+- send message to chat
 ### Code Golf
-- **timer**
-- ~~**character counter**~~
+- ~~timer~~
+    - last lap font
+    - pause
+    - add/remove time
+    - restart
+    - improve accuracy
+- ~~character counter~~
     - ~~needs more hair~~
 ### Croud Control
 - letter inventory
@@ -16,12 +21,16 @@ All notable changes to the "twitch-golf" extension will be documented in this fi
 - disable keys
 - black screen while typing
 
-## 2.0.0 - 2020-05-06
+## 0.0.1 - 2020-05-11
+### Added
+- Timer
+
+## 0.0.1 - 2020-05-06
 ### Removed
 - Character counter status bar item
 ### Added
 - Codelens character counter
 
-## 1.0.0 - 2020-04-30
+## 0.0.1 - 2020-04-30
 ### Added
 - Character counter status bar item
--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,59 @@
 import * as vscode from 'vscode';
 import { CharacterCountCodelensProvider } from './characterCountLens'
 
+let secondsLeft: number;
+let timer: NodeJS.Timeout;
+
+let timerStartButton: vscode.StatusBarItem;
+let timerDisplay: vscode.StatusBarItem;
+
+function updateTimerDisplay() {
+	const hours = Math.floor(secondsLeft / 60 / 60).toString().padStart(2, "0");
+	const minutes = Math.floor(secondsLeft / 60 % 60).toString().padStart(2, "0");
+	const seconds = Math.floor(secondsLeft % 60).toString().padStart(2, "0");
+	timerDisplay.text = `${hours}:${minutes}:${seconds}`
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	vscode.languages.registerCodeLensProvider("*", new CharacterCountCodelensProvider())
+
+	const startTimerId = 'twitch-golf.startTimer';
+	context.subscriptions.push(vscode.commands.registerCommand(startTimerId, async () => {
+		const input = vscode.window.showInputBox({
+			prompt: "Type the number of minutes you want to set a timer for.",
+			validateInput: input => {
+				const minutes = Number(input);
+				if (!Number.isInteger(minutes) || minutes <= 0) {
+					return "Please input a positive integer"
+				}
+			}
+		});
+		secondsLeft = Number(await input) * 60;
+		updateTimerDisplay();
+		timerDisplay.show();
+		clearInterval(timer);
+		timer = setInterval(() => {
+			secondsLeft--;
+			if (secondsLeft === 0) {
+				timerDisplay.hide();
+				clearInterval(timer);
+				vscode.window.showInformationMessage("Game over!");
+			} else {
+				updateTimerDisplay();
+			}
+		}, 1000);
+	}));
+
+	timerStartButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, Number.MAX_SAFE_INTEGER);
+	timerStartButton.command = startTimerId;
+	timerStartButton.tooltip = "Start a golf timer"
+	timerStartButton.text = "$(play)";
+	context.subscriptions.push(timerStartButton);
+	timerStartButton.show();
+
+	timerDisplay = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, Number.MAX_SAFE_INTEGER);
 }
 
 // this method is called when your extension is deactivated
