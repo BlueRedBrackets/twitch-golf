f2bb977f08d8c547a75f74cc188d736fdc83ee2f
blueredbrackets <blueredbrackets@gmail.com>
2020-05-25 12:30:27 -0500

    refactored character counter

--- src/characterCountLens.ts
+++ src/characterCountLens.ts
@@ -1,29 +1,20 @@
 import * as vscode from 'vscode';
 
-function createLens(lineNumber: number, characterCount: number): vscode.CodeLens {
-    const position = new vscode.Position(lineNumber, 0);
-    const lens = new vscode.CodeLens(new vscode.Range(position, position));
-    lens.command = {
-        title: `char count: ${characterCount.toLocaleString()}`,
-        command: ""
-    };
-    return lens
-}
-
 export class CharacterCountCodelensProvider implements vscode.CodeLensProvider {
+    private codeBlockRegex = new RegExp(/(?:[^\n]\n{0,2})+/, "g")
     public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
         const lenses: vscode.CodeLens[] = [];
-        let blockCharacterCount: number = 0;
-        for (let i = document.lineCount - 1; i >= 0; i--) {
-            const line = document.lineAt(i);
-            if (!line.isEmptyOrWhitespace) blockCharacterCount += line.text.length + 1;
-            if (i == 0 && blockCharacterCount > 0) {
-                lenses.push(createLens(i, blockCharacterCount - 1));
-            } else if (line.isEmptyOrWhitespace && blockCharacterCount > 0) {
-                lenses.push(createLens(i + 1, blockCharacterCount - 1));
-                blockCharacterCount = 0;
+        const documentText = document.getText();
+        let match;
+        while ((match = this.codeBlockRegex.exec(documentText))) {
+            const position = document.positionAt(match.index)
+            const lens = new vscode.CodeLens(new vscode.Range(position, position));
+            lens.command = {
+                title: `char count: ${match[0].trim().length.toLocaleString()}`,
+                command: ""
             }
+            lenses.push(lens);
         }
-        return lenses;
+        return lenses
     }
 }
